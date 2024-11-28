6f5a80971d3b958d0a95898a673a1b8bc0e480f0
vrinda040104 <vrinda885@gmail.com>
2024-11-28 12:10:55 +0530

    Initial commit for Crafted Connections project

---
+++ server/database.js
@@ -0,0 +1,12 @@
+let { MongoClient } = require('mongodb');
+const url = "mongodb://localhost:27017";
+const clientObj = new MongoClient(url);
+let db;
+try {
+	db = clientObj.db("grocery");
+}
+catch (err) {
+	console.log(err);
+	throw err;
+}
+module.exports = db;
